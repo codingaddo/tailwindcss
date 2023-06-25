f5e660b1b6e189392682896fb8b7edc2c76490b9
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-06-25 02:24:19 +0000

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+const darkIcon = document.querySelector('.moon')
+const lightIcon = document.querySelector('.sun')
+
+
+
+darkIcon.addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.documentElement.classList.toggle('dark')
+    darkIcon.classList.toggle('hidden')
+})
+
+lightIcon.addEventListener('click',(event)=>{
+    event.preventDefault()
+    document.documentElement.classList.toggle('dark')
+    darkIcon.classList.remove('hidden')
+})
+
+
 btn.addEventListener('click',()=>{
     btn.classList.toggle('open')
     nav.classList.toggle('hidden')
